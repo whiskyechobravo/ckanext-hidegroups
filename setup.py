9ab904803c7e746a75f162934d446868cffdc450
David Lesieur <david@davidlesieur.com>
2022-03-25 18:34:19 -0400

    Update for CKAN 2.9.

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
-
 setup(
     name='ckanext-hidegroups',
-    version=version,
+    version='0.2',
     description="A CKAN 2 plugin that hides the groups feature from the web interface",
     long_description="""""",
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='Sean Hammond, Bart Saelen',
+    keywords='''CKAN''',
+    author='Sean Hammond, Bart Saelen, David Lesieur',
     author_email='',
     url='',
     license='',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext', 'ckanext.hidegroups'],
+    namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
     ],
-    entry_points= \
-    """
+    entry_points='''
         [ckan.plugins]
-        # Add plugins here, eg
         hidegroups=ckanext.hidegroups.plugin:HideGroupsPlugin
-    """,
+    ''',
 )
